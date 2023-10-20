fb3a7a86f9cf5fb6cd77bea57066d088a38da8bd
Joey Palchak <joey.palchak@gmail.com>
2023-10-20 09:40:21 -0700

    add Create & Details actions + Views for Treats

--- Pierres/Controllers/TreatsController.cs
+++ Pierres/Controllers/TreatsController.cs
@@ -24,7 +24,37 @@ namespace Pierres.Controllers
 
     public ActionResult Index()
     {
+      List<Treat> treats = _db.Treats.ToList();
+      return View(treats);
+    }
+
+    public ActionResult Create()
+    {
       return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Treat treat)
+    {
+      if(!ModelState.IsValid)
+      {
+        return View(treat);
+      }
+      else
+      {
+        _db.Treats.Add(treat);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+    }
+
+    public ActionResult Details(int id)
+    {
+      Treat thisTreat = _db.Treats
+                                  .Include(treat => treat.JoinEntities)
+                                  .ThenInclude(join => join.Flavor)
+                                  .FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
     }
   }
 }
