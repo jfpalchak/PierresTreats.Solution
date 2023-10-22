2952b69de59352f2fd150b4508e1df1770baf2ce
Joey Palchak <joey.palchak@gmail.com>
2023-10-22 12:38:39 -0700

    remove overrides for default password config

--- Pierres/ViewModels/RegisterViewModel.cs
+++ Pierres/ViewModels/RegisterViewModel.cs
@@ -11,8 +11,7 @@ namespace Pierres.ViewModels
 
     [Required]
     [DataType(DataType.Password)]
-    // ! PUT BACK BEFORE SUBMISSION
-    // [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,}$", ErrorMessage = "Your password must contain at least six characters, a capital letter, a lowercase letter, a number, and a special character.")]
+    [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,}$", ErrorMessage = "Your password must contain at least six characters, a capital letter, a lowercase letter, a number, and a special character.")]
     public string Password { get; set; }
 
     [Required]
