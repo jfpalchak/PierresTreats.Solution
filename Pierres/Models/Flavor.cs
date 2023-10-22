dbbd9319a36404c973f3ed59c7e953fec906e3df
Joey Palchak <joey.palchak@gmail.com>
2023-10-22 12:37:29 -0700

    add asterisk to validation error message

--- Pierres/Models/Flavor.cs
+++ Pierres/Models/Flavor.cs
@@ -6,7 +6,7 @@ namespace Pierres.Models
   public class Flavor 
   {
     public int FlavorId { get; set; }
-    [Required(ErrorMessage = "The flavor's Type can't be empty!")]
+    [Required(ErrorMessage = "* The flavor's Type can't be empty!")]
     public string Type { get; set; }
 
     public List<TreatFlavor> JoinEntities { get; set; }
